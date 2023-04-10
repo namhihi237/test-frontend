7429a322de2fdd9dabbf528a6b01a7ec691c5eef
Nam Le <yentth237@gmail.com>
2023-04-11 00:11:17 +0700

    fix: font

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <div className='flex justify-between px-5 py-6 bg-[#262626]'>
-        <p className={`text-[24px] pr-4 font-sans font-bold text-white`}>Choose the form of payment</p>
+      <div className='flex justify-between px-5 py-8 bg-[#262626]'>
+        <p className={`text-[24px] pr-8 font-sans font-bold text-white`}>Choose the form of payment</p>
         <div>
           <button>
             <Image src="/cancel.png" alt='cancel' width={18} height={18} className='text-black pt-3' />
@@ -22,7 +22,7 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <button className='bg-[#EDF2F7] px-4 py-4 rounded-[8px] font-medium text-[16px]'>Log out</button>
+            <button className='bg-[#EDF2F7] px-4 py-4 rounded-[8px] font-semibold text-[16px]'>Log out</button>
           </div>
         </div>
 
@@ -34,7 +34,7 @@ export default function Home() {
               <Image src="/avt.png" alt='cancel' width={55} height={55} className='text-black rounded-md mr-3' />
             </div>
             <div className='px-4 py-4 '>
-              <p className='font-medium text-[16px] mb-1'>Japanese lessons</p>
+              <p className='font-semibold text-[16px] mb-1'>Japanese lessons</p>
               <div className='flex text-[#718096] mb-1' >
                 <Image src="/calendar.svg" alt='cancel' width={12} height={12} className='text-black rounded-md' />
                 <p className='text-[10px] ml-2'>Nov 7, 2020 · 11:30</p>
@@ -43,32 +43,32 @@ export default function Home() {
                 <Image src="/place.svg" alt='cancel' width={12} height={12} className='text-black rounded-md' />
                 <p className='text-[10px]  ml-2'>Client`s place</p>
               </div>
-              <p className='text-[16px]'>Rp 350.000</p>
+              <p className='text-[16px] font-semibold'>Rp 350.000</p>
             </div>
           </div>
         </div>
 
         {/* location */}
-        <p className='font-medium mt-6 mb-2'>Enter your location</p>
+        <p className='font-semibold mt-6 mb-2'>Enter your location</p>
         <input type='text' className='w-full border rounded-[10px] h-12 px-3 placeholder-[#D3DEE9]' placeholder='Enter address' />
 
-        <p className='font-medium mt-6 mb-2'>Note (optional)</p>
+        <p className='font-semibold mt-6 mb-2'>Note (optional)</p>
         <input type='text' className='w-full border rounded-[10px] h-12 px-3 placeholder-[#D3DEE9]' placeholder='Enter text here' />
 
         {/* Choose pay */}
         <p className='text-[24px] font-bold mt-6 mb-6'>Choose a way to pay</p>
         <div class="w-full flex justify-between">
           <div class="flex-none w-[50%] border border-[#262626] mr-2 rounded-[10px] py-3 flex justify-center">
-            <p className='font-medium'>Cash</p>
+            <p className='font-semibold'>Cash</p>
           </div>
           <div class="flex-grow-0 w-[50%] ml-2 py-3 flex justify-center rounded-[10px] bg-[#EDF2F7]">
-            <p className='font-medium'>Credit card</p>
+            <p className='font-semibold'>Credit card</p>
           </div>
         </div>
         <div class="mt-4">
           <div class="flex-grow w-[50%] ml-2 py-1 rounded-[10px] bg-[#EDF2F7]">
-            <p className='font-medium flex items-center justify-center'>eWallet/</p>
-            <p className='font-medium flex items-center justify-center'>Virtual bank</p>
+            <p className='font-semibold flex items-center justify-center'>eWallet/</p>
+            <p className='font-semibold flex items-center justify-center'>Virtual bank</p>
           </div>
         </div>
 
@@ -88,8 +88,8 @@ export default function Home() {
 
       {/* bottom */}
       <div className='flex justify-between items-center border-t-2 py-4 px-6'>
-        <a href='\'>Previous</a>
-        <button className='px-10 text-white bg-[#1E21FF] py-3 rounded-[10px]'>Next</button>
+        <a className='font-medium' href='\'>Previous</a>
+        <button className='px-10 text-white bg-[#1E21FF] py-3 rounded-[10px] font-semibold'>Next</button>
       </div>
     </div>
   )
